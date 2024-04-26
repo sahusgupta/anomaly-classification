bd06746f0e00d104ae701ba0ee0dabf3d159acf6
MambaAllDay24 <sahusgupta@gmail.com>
2024-04-26 02:03:46 -0500

    done!!!

--- testing_model.py
+++ testing_model.py
@@ -1,16 +1,28 @@
-from sklearn import *
 import pandas as pd
-from sklearn import metrics
+from sklearn import metrics, preprocessing
 from training import model
 
+# load the dataset and  initialize object encoder
 network = pd.read_csv("data/testing.csv", header=0)
-
-cols = ["SrcAddr","DstAddr","Mean","Sport","Dport","SrcPkts","DstPkts","TotPkts","DstBytes","SrcBytes","TotBytes","SrcLoad","DstLoad","Load","SrcRate","DstRate","Rate","SrcLoss","DstLoss","Loss","pLoss","SrcJitter","DstJitter","SIntPkt","DIntPkt","Proto","Dur","TcpRtt","IdleTime","Sum","Min","Max","sDSb","sTtl","dTtl","sIpId","dIpId","SAppBytes","DAppBytes","TotAppByte","SynAck","RunTime","sTos","SrcJitAct","DstJitAct","Target"]
+encoder  = preprocessing.LabelEncoder()
+# iterate through columns in network and transform non-integer values to doubles
+for col in network.columns:
+    if network[col].dtype == object:
+        network[col] = encoder.fit_transform(network[col])
+    else:
+        continue
+    
+# columns of interest
+cols = ["StartTime", "LastTime", "SrcAddr","DstAddr","Mean","Sport","Dport","SrcPkts","DstPkts","TotPkts","DstBytes","SrcBytes","TotBytes","SrcLoad","DstLoad","Load","SrcRate","DstRate","Rate","SrcLoss","DstLoss","Loss","pLoss","SrcJitter","DstJitter","SIntPkt","DIntPkt","Proto","Dur","TcpRtt","IdleTime","Sum","Min","Max","sDSb","sTtl","dTtl","sIpId","dIpId","SAppBytes","DAppBytes","TotAppByte","SynAck","RunTime","sTos","SrcJitAct","DstJitAct","Traffic","Target"]
+# initialize the dataframe and load all columns (except whether the traffic is normal or anomaly) and rows
 df = pd.DataFrame(network)
-# load all data in the dataset
 x = df.loc[:, cols]
-y = df.Traffic
 
+# want to predict whether a piece of traffic is anomaly or normal, encoded as 1 or 2
+y = df.Traffic
+# test the model using the loaded information
 classifications = model.predict(x)
+# compute accuracy metrics
 accuracy = metrics.accuracy_score(y, classifications)
 print(accuracy)
+
--- training.py
+++ training.py
@@ -1,14 +1,25 @@
-from sklearn import linear_model
+from sklearn import linear_model, preprocessing
 import pandas as pd
 
+# load the training dataset and initialize the object encoder
 network = pd.read_csv("data/training.csv", header=0)
+encoder = preprocessing.LabelEncoder()
 
-cols = ["SrcAddr","DstAddr","Mean","Sport","Dport","SrcPkts","DstPkts","TotPkts","DstBytes","SrcBytes","TotBytes","SrcLoad","DstLoad","Load","SrcRate","DstRate","Rate","SrcLoss","DstLoss","Loss","pLoss","SrcJitter","DstJitter","SIntPkt","DIntPkt","Proto","Dur","TcpRtt","IdleTime","Sum","Min","Max","sDSb","sTtl","dTtl","sIpId","dIpId","SAppBytes","DAppBytes","TotAppByte","SynAck","RunTime","sTos","SrcJitAct","DstJitAct","Traffic","Target"]
-
+# iterate through columns in network and transform non-integer values to doubles
+for col in network.columns:
+    if network[col].dtype == object:
+        network[col] = encoder.fit_transform(network[col])
+    else:
+        continue
+    
+# columns of interest
+cols = ["StartTime", "LastTime","SrcAddr","DstAddr","Mean","Sport","Dport","SrcPkts","DstPkts","TotPkts","DstBytes","SrcBytes","TotBytes","SrcLoad","DstLoad","Load","SrcRate","DstRate","Rate","SrcLoss","DstLoss","Loss","pLoss","SrcJitter","DstJitter","SIntPkt","DIntPkt","Proto","Dur","TcpRtt","IdleTime","Sum","Min","Max","sDSb","sTtl","dTtl","sIpId","dIpId","SAppBytes","DAppBytes","TotAppByte","SynAck","RunTime","sTos","SrcJitAct","DstJitAct","Traffic","Target"]
+# initialize the dataframe
 df = pd.DataFrame(network)
 # load all data in the dataset
 x = df.loc[:, :]
 y = df.Traffic
 
-model = linear_model.LogisticRegression()
+# train the model, then use the trained model to predict in testing_model.py
+model = linear_model.LogisticRegression(max_iter = 10**7)
 model.fit(x, y)
